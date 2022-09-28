5e11cae087e93a6ee79736f28538062c011176f0
Allan Masiga <zionalehebrew@gmail.com>
2022-09-28 11:54:14 -0700

    returns 1 if a string is a palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -5,7 +5,7 @@ int check_palindrome(char *s, int len, int index);
 int is_palindrome(char *s);
 
 /**
- * find_strlen returns length of a string.
+ * find_strlen - Returns the length of a string.
  * @s: The string to be measured.
  *
  * Return: The length of the string.
