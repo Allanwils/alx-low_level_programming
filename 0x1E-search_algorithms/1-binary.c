6402e1545b16af8926d46a1c4b6fca5730295f4e
Allan Masiga <zionalehebrew@gmail.com>
2023-04-12 07:06:18 -0700

    try task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,48 +1,58 @@
 #include "search_algos.h"
 
+int recurse_helper(int *array, size_t left, size_t right, int value);
+
 /**
-  * binary_search - Does binary search
-  * @array: points to first element.
-  * @size: size of array
-  * @value: value to find
-  *
-  * Return: index where found or -1
-  */
+ * binary_search - binary search.
+ * @array: array to search
+ * @size: size of array
+ * @value: value to search
+ *
+ * Return: index of found value; or -1 if not found
+ */
 int binary_search(int *array, size_t size, int value)
 {
-	int  left, right, pivot;
-
 	if (array == NULL)
 		return (-1);
-	left = 0;
-	right = size - 1;
-	while (left <= right)
-	{
-		print_array(array, left, right);
-		pivot = (left + right) / 2;
-		if (array[pivot] > value)
-			right = pivot - 1;
-		else if (array[pivot] < value)
-			left = pivot + 1;
-		else
-			return (pivot);
-	}
-	return (-1);
+
+	return (recurse_helper(array, 0, size - 1, value));
 }
 
 /**
-  * print_array - print array
-  * @array: array
-  * @left: starting print
-  * @right: ending print
-  */
-
-void print_array(int *array, int left, int right)
+ * recurse_helper - recursive implement of binary search
+ * @array: array to search
+ * @left: leftmost index
+ * @right: rightmost index
+ * @value: value to search
+ *
+ * Return: index of found value; or -1 if not found
+ */
+int recurse_helper(int *array, size_t left, size_t right, int value)
 {
-	int i;
+	size_t i = left, mid;
 
-	printf("Searching in array: ");
-	for (i = left; i < right; i++)
-		printf("%d, ", array[i]);
-	printf("%d\n", array[i]);
+	if (left > right)
+		return (-1);
+
+	/* print search progress */
+	printf("Searching in array: %d", array[i++]);
+	while (i <= right)
+		printf(", %d", array[i++]);
+	printf("\n");
+
+	/* calculate mid */
+	mid = left + ((right - left) / 2);
+
+	/* check if mid is value */
+	if (array[mid] == value)
+		return (mid);
+	else if (array[mid] > value)
+	{
+		if (mid != 0)
+			return (recurse_helper(array, left, mid - 1, value));
+		else
+			return (-1);
+	}
+	else
+		return (recurse_helper(array, mid + 1, right, value));
 }
