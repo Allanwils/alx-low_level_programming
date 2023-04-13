62afbf3f172b1a5a05862d922656347b4f5d336d
Allan Masiga <zionalehebrew@gmail.com>
2023-04-13 01:11:14 -0700

    Do it

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,23 +1,3 @@
-#include "search_algos.h"
-
-int recurse_helper(int *arr, size_t size, int val, size_t idx);
-
-/**
- * linear_search - Linear search for value in integer array
- * @array: points to array of integers
- * @size: size of array
- * @value: value to search for
- *
- * Return: index of value; -1 if value isn't found.
- */
-int linear_search(int *array, size_t size, int value)
-{
-	if (array == NULL)
-		return (-1);
-
-	return (recurse_helper(array, size, value, 0));
-}
-
 /**
  * recurse_helper - Recursive implementation of linear search
  * @arr: points to array of integers
@@ -32,15 +12,13 @@ int recurse_helper(int *arr, size_t size, int val, size_t idx)
 	if (arr == NULL || (long int)size < 0)
 		return (-1);
 
-	for (size_t i = 0; i < size; i++)
+	if (arr[idx] == val)
 	{
-		printf("Value checked array[%lu] = [%d]\n", i, arr[i]);
-
-		if (arr[i] == val)
-		{
-			return ((int)i);
-		}
+		return ((int)idx);
 	}
-
-	return (recurse_helper(arr, size, val, idx + 1));
+	else if (idx == size - 1)
+	{
+		return (-1);
+	}
+	
 }
